439006f0506e9cf619c1a87f4283fc7abb114098
sushii-cmyk <danielalfredwilkerson@gmail.com>
2026-06-15 15:24:13 -0500

    added overload!

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
-HYDRO = "Hydro"
-PYRO = "Pyro"
 
 class Gauge:
+    HYDRO = "Hydro"
+    PYRO = "Pyro"
+    ELECTRO = "Electro"
+    
     def __init__(self, element, units):
         self.element = element
         self.units = units
@@ -14,27 +16,41 @@ class Gauge:
     def __str__(self):
         return f"({self.units} {self.element})"
 
+    def __bool__(self):
+        return bool(self.units)
+
     element = None
     units = 0
 
 class Pyro(Gauge):
     def react(self, incoming):
+        prev = f"{incoming}, {self}"
+        reaction = "()"
         match (incoming.element):
-            case HYDRO:
-                prev = f"{incoming}, {self}"
-
+            case Gauge.HYDRO:
+                reaction = "hV"
                 coef = 2
-
                 temp = self.units
                 self -= coef * incoming.units
                 incoming -= temp / coef
+                
+            case Gauge.ELECTRO:
+                reaction = "eO"
+                coef = 1
+                temp = self.units
+                self -= coef * incoming.units
+                incoming -= temp
 
-                print(f"VAPE: {prev} -> {incoming}, {self};", end = " ")
+
+        print(f"{reaction}: {prev} > {self}{f' +> {incoming}' if incoming else ''}")
 
         return self, incoming
 
     def __str__(self):
         return f"({self.units} {self.element})"
+
+    def __bool__(self):
+        return self.units
 
 class Aura:
     existing = []
@@ -43,7 +59,7 @@ class Aura:
         return "[" + ",".join(str(e) for e in self.existing) + "]"
 
     def manageGauges(self, existing, incoming):
-
+        print(f"resolving {incoming} > [{self}]")
         for e in existing:
             e, remaining = e.react(incoming)
             if remaining.units == 0:
@@ -63,22 +79,23 @@ class Aura:
             self.existing.append(incoming)
         else:
             self.existing, remaining = self.manageGauges(self.existing, incoming)
-            #if remaining.units > 0:
-            #    self.apply(existing, remaining)
+            if remaining.units > 0:
+                self.apply(remaining)
         
-        print(f"[{prev} -> {self}]")
+        print(f"previously: {prev} -> now: {self}")
        
 
 
 def main():
     aura = Aura()
-    hydro = Gauge(HYDRO, 2)
-    pyro = Pyro(PYRO, 2)
+    hydro = Gauge(Gauge.HYDRO, 2)
+    electro = Gauge(Gauge.ELECTRO, 1)
+    pyro_aura = Pyro(Gauge.PYRO, 2)
 
-    print(hydro)
+    #print(hydro)
 
-    aura.apply(pyro)
-    aura.apply(hydro)
+    aura.apply(pyro_aura)
+    aura.apply(electro)
 
 if __name__ == "__main__":
     main()
